3907d49f3ba8ed3723abef155da48495ba95deb4
jonatas almeida <jonatas.olialmeida@gmail.com>
2023-06-11 22:27:58 -0300

    adicionamos um modulo de services para fazer a comunicação com a futura API, por enquanto utilizando um json + doi add um loading

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Course } from '../model/course';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -6,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
+  courses$: Observable<Course[]>;
+  displayedColumns = ['name', 'category'];
+  // coursesService: CoursesService;
 
+  constructor(private coursesService: CoursesService) {
+    // this.coursesService = new CoursesService();
+    this.courses$ = this.coursesService.list();
+  }
 }
